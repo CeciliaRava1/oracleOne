c5d1b8c0ff0c8e47d0e40a8e56d082f243ce32b5
Cecilia <ravacecilia1@gmail.com>
2025-01-15 14:03:30 -0300

    feat: Create the game 'guess the number'

---
+++ programming-logic-dive-into-javascript/guess-the-number/app.js
@@ -0,0 +1,36 @@
+const numberToGuess = Math.round(Math.random() * (10 - 1) + 1);
+numberOfAttempts = 5;
+correctInput = false;
+playerGuess = false;
+
+while (correctInput != true) {
+
+    while (numberOfAttempts > 0) {
+
+        let inputNumber = prompt("Enter a number between 1-10","0");
+
+        if (inputNumber >= 1 && inputNumber <= 10) {
+            correctInput = true;
+            numberOfAttempts -= 1;
+        } else {
+            alert('Your input is not between 1-10. Try again');
+        }
+
+        if (inputNumber == numberToGuess) {
+            alert(`You guess! :) The number is ${numberToGuess}`);
+            playerGuess = true;
+            break;
+        } else if (inputNumber > numberToGuess) {
+            alert('Number to guess is smaller. Try again');
+        } else if (inputNumber < numberToGuess) {
+            alert('Number to guess is higher. Try again');
+        }
+    }
+
+    if (numberOfAttempts == 0 && playerGuess == false) {
+        alert('You do not have more attempts. You lost');
+    }
+
+}
+
+
