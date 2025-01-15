b944fde09bd23f2e0cf8e994373f171020f4976d
Cecilia <ravacecilia1@gmail.com>
2025-01-15 10:59:51 -0300

    feat: Add solved exercises from the section 01-09

---
+++ programming-logic-dive-into-javascript/exercises/script.js
@@ -0,0 +1,35 @@
+// 1. Show an alert with the message 'Welcome to our website!'
+alert('Welcome to our website!');
+
+// 2. Declare a variable 'nameInput' and assign it the value 'Lua'
+let nameInput = 'Lua';
+
+// 3. Create a variable 'age' and assign it the value 25
+let age = 25;
+
+// 4. Establish a variable 'amountOfSales' and assign it the value 50
+let amountOfSales = 50;
+
+// 5. Establish a variable 'availableBalance' and assign it the value 1000
+let availableBalance = 1000;
+
+// 6. Show an alert with the text 'Error! Complete all the fields'
+alert('Error! Complete all the fields');
+
+// 7. Declare a variable 'errorMessage' and assign it the value 'Error! Complete all the fields'
+let errorMessage = 'Error! Complete all the fields';
+
+// Now, show an alert with the value of the variable 'errorMessage'
+alert(errorMessage)
+
+// 8. Use a prompt to ask the name of the user, and save it in the variable 'nameUser'
+let nameUser = prompt('Enter your name', 'Type here');
+
+// 9. Ask for the age of the user using a prompt, and save it in the variable 'ageUser'
+let ageUser = prompt('Enter your age', 'Type here');
+
+// // 10. Now, if the age is higher or equal to 18, show an alert with the message 'You can obtain
+// your driver license!'
+if (ageUser >= 18) {
+    alert("You can obtain your driver's license!");
+}
