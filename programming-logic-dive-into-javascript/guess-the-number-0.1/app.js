b98f8b5667e0d0986ea9507b8157a03f577bce45
Cecilia <ravacecilia1@gmail.com>
2025-01-16 15:26:34 -0300

    feat: Progress with the game 'guess the number v 0.1'

---
+++ programming-logic-dive-into-javascript/guess-the-number-0.1/app.js
@@ -0,0 +1,5 @@
+let title = document.querySelector('h1');
+title.innerHTML = 'Guess the number!';
+
+let paragraph = document.querySelector('.paragraph');
+paragraph.innerHTML = 'Enter a number between 1-10';
