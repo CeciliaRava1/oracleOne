8c94a8e6a2e0e380719e5be5dcbff888282e64f3
Cecilia <ravacecilia1@gmail.com>
2025-01-20 11:31:29 -0300

    feat: Add functionality to the buttons

--- programming-logic-dive-into-javascript/guess-the-number-0.1/app.js
+++ programming-logic-dive-into-javascript/guess-the-number-0.1/app.js
@@ -1,9 +1,5 @@
 let secretNumber = generateSecretNumber();
-
-function verifyAttempt() {
-    let userAttempt = parseInt(document.querySelector('.container_input').value);
-    return;
-}
+let numberOfAttempts = 5;
 
 function assignTextToElement(element, text) {
     let elementHTML = document.querySelector(element);
@@ -14,53 +10,56 @@ assignTextToElement('h1', 'Guess the number');
 assignTextToElement('.paragraph', 'Enter a number between 1-10');
 
 function generateSecretNumber() {
-   return Math.round(Math.random() * (10) + 1);
+    return Math.round(Math.random() * 10 + 1);
+}
+
+function cleanInput() {
+    document.querySelector('.container_input').value = '';
+}
+
+cleanInput();
+
+function verifyAttempt() {
+    let playerGuess = false;
+   
+    while (numberOfAttempts > 0 && !playerGuess) {
+        let userAttempt = parseInt(document.querySelector('.container_input').value);
+
+        if (userAttempt >= 1 && userAttempt <= 10) {
+            numberOfAttempts -= 1;
+
+            if (userAttempt == secretNumber) {
+                assignTextToElement('.paragraph', `You guessed it! :) The number is ${secretNumber}`);
+                playerGuess = true;
+                break;
+            } else if (userAttempt > secretNumber) {
+                assignTextToElement('.paragraph', 'The number is smaller. Try again.');
+                cleanInput();
+                break;
+            } else if (userAttempt < secretNumber) {
+                assignTextToElement('.paragraph', 'The number is higher. Try again.');
+                cleanInput();
+                break;
+            }
+        } else {
+            assignTextToElement('.paragraph', 'Your input is not between 1-10. Try again.');
+            cleanInput();
+            break;
+        }
+
+        if (numberOfAttempts == 0 && !playerGuess) {
+            assignTextToElement('.paragraph', 'You do not have more attempts. You lost.');
+            cleanInput();
+        }
+    }
 }
 
 
 
-// let startButton = document.querySelector('.container_button');
-// startButton.addEventListener('click');
 
 
 
 
-
-
-// let numberOfAttempts = 5;
-// let correctInput = false;
-// let playerGuess = false;
-// let inputNumber = 0;
-
-// while (correctInput != true) {
-
-//     while (numberOfAttempts > 0) {
-
-//         let inputNumber = prompt("Enter a number between 1-10","0");
-
-//         if (inputNumber >= 1 && inputNumber <= 10) {
-//             correctInput = true;
-//             numberOfAttempts -= 1;
-//         } else {
-//             alert('Your input is not between 1-10. Try again');
-//         }
-
-//         if (inputNumber == numberToGuess) {
-//             alert(`You guess! :) The number is ${numberToGuess}`);
-//             playerGuess = true;
-//             break;
-//         } else if (inputNumber > numberToGuess) {
-//             alert('Number to guess is smaller. Try again');
-//         } else if (inputNumber < numberToGuess) {
-//             alert('Number to guess is higher. Try again');
-//         }
-//     }
-
-//     if (numberOfAttempts == 0 && playerGuess == false) {
-//         alert('You do not have more attempts. You lost');
-//     }
-
-// }
 
 
 
